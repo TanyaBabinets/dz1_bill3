6ba47ff11e9e81af85d914a4a5276ab8c9ddbce6
TanyaBabinets <tanya060616@gmail.com>
2024-09-13 20:42:51 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+﻿
+import { Component } from '@angular/core';
+
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+@Component({
+	selector: 'my-app',
+	standalone: true,
+	imports: [FormsModule, CommonModule],
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+
+	section: string | null = null; //стартовая страница по умолчанию просит выбор
+	showSection(sec: string): void {
+		this.section = sec;
+	}
+}
+
+
+
+
+
+
